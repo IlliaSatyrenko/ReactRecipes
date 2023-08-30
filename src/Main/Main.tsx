293c48f095563f2e68b61d558d21f3ec59733851
IlliaSatyrenko <denzp05@gmail.com>
2023-08-31 02:27:18 +0300

    fixed routing

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -8,9 +8,9 @@ function Main() {
         <main style={{ minHeight: "100vh", backgroundColor: "#fff6ea" }}>
             <BrowserRouter basename="/Recipes">
                 <Routes>
-                    <Route path="/Recipes" element={<Navigate to="all-items" />} />
-                    <Route path="item/:id" element={<ItemPage />} />
                     <Route path="all-items" element={<AllItemsPage />} />
+                    <Route path="item/:id" element={<ItemPage />} />
+                    {/* <Route path="all-items" element={<AllItemsPage />} /> */}
                     <Route path="*" element={<InvalidPath />} />
                 </Routes>
             </BrowserRouter>
