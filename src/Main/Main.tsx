9fef3d9b9c7609cf5d31ad098c93cc68f2bd7f3a
IlliaSatyrenko <denzp05@gmail.com>
2023-08-31 12:08:33 +0300

    fixed routing

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,19 +1,19 @@
 import AllItemsPage from "./AllItemsPage/AllItemsPage";
 import InvalidPath from "./InvalidPath";
 import ItemPage from "./ItemPage/ItemPage";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 
 function Main() {
     return (
         <main style={{ minHeight: "100vh", backgroundColor: "#fff6ea" }}>
-            <BrowserRouter basename="/Recipes">
+            <HashRouter basename="/Recipes">
                 <Routes>
                     <Route path="/" element={<Navigate to={"all-items"} />} />
                     <Route path="item/:id" element={<ItemPage />} />
                     <Route path="all-items" element={<AllItemsPage />} />
                     <Route path="*" element={<InvalidPath />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </main>
     );
 }
